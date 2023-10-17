3b19e98c40cf4666f84f5ede39baf67f9b402fa8
Raluca Antal-Vaida <ralucaantal0609@gmail.com>
2023-10-17 18:59:43 +0300

    draft login

---
+++ Login/Login.js
@@ -0,0 +1,4 @@
+export function login(e) {
+    console.log("Vrei sa te autentifici");
+    e.preventDefault();
+  }
--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-console.log("Buna ziua");
+let users = [
+  {
+    id: 1,
+    name: "Mihai",
+    email: "mihai@gmail.com",
+    password: "Mihai1!",
+  },
+  {
+    id: 2,
+    name: "Raluca",
+    email: "raluca@gmail.com",
+    password: "Raluca6!",
+  },
+];
+
+let toDos = [
+  {
+    id: 1,
+    taskName: "Do the dishes!",
+    status: "to-do",
+    responsable: "Mihai",
+  },
+  {
+    id: 2,
+    taskName: "Do the laundry!",
+    status: "to-do",
+    responsable: "Mihai",
+  },
+  {
+    id: 3,
+    taskName: "Do your homework",
+    status: "in-progress",
+    responsable: "Raluca",
+  },
+  {
+    id: 4,
+    taskName: "binging",
+    status: "to-do",
+    responsable: "Raluca",
+  },
+];
+
+import { login } from "./Login/Login.js";
+
+const loginButton = document.getElementById("loginSubmit");
+loginButton.addEventListener("click", login);
